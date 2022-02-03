3070b74848db41dd71c5b5ddde9f1d3558ec460d
lucyb-work <lucybpersonalemail@gmail.com>
2022-02-03 15:57:14 +0000

    changed files for organisation

---
+++ variables.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() {
+
+    // basic print statement
+    printf("Hello world!\n");
+
+    /* make constants by using const before type.
+    use all caps naming convention*/
+    const int HEIGHT = 163;
+    int intA = 97;
+    char targetChar;
+    double targetDouble;
+
+    /* insert vars into print statments using %[x]
+    [x] representing the variable type */
+    printf("My height is:\t%icm", HEIGHT);
+
+    // type casting exists too, both implicitely and explicitly
+    targetDouble = HEIGHT;
+    targetChar = (char)intA;
+    printf("\nThe value %i is %c in character form", intA, targetChar);
+
+    
+
+
+    return 0;    
+}
