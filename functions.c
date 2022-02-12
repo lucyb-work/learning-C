ca201e1d4875a6bc6967e19980551b82ac5eec07
Luce <76128271+lucyb-work@users.noreply.github.com>
2022-02-12 16:54:48 +0000

    inital functions.c commit
    nowhere near finished

---
+++ functions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[])
+{
+    // some spicy number functions
+    int negative = -5;
+    // abs() - returns absolute value
+    printf("%i", abs(negative));
+    // ceil() - returns the roundup of a num
+    double decimal = 3.1;
+    printf("%i", ceil(decimal));
+
+
+    return 0;
+}
